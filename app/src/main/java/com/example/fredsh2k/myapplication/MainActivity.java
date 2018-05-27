252abe98ad18ea62c69ed78d9f393dec4f2fc4e8
fredsh2k <fredsh2k@gmail.com>
2018-05-27 19:04:48 +0300

    PART1 problem adding points

--- app/src/main/java/com/example/fredsh2k/myapplication/MainActivity.java
+++ app/src/main/java/com/example/fredsh2k/myapplication/MainActivity.java
@@ -5,15 +5,22 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int points = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        ((TextView)findViewById(R.id.pointsTextView)).setText(points);
+
+
 
         MediaPlayer ring= MediaPlayer.create(MainActivity.this,R.raw.bravesong);
         ring.start();
@@ -27,10 +34,26 @@ public class MainActivity extends AppCompatActivity {
     public void goToPart1(View view) {
         Intent intent = new Intent(this, Part1.class);
         startActivity(intent);
+
+
+
     }
 
     public void goToPart2(View view) {
         Intent intent = new Intent(this, activity_part2.class);
         startActivity(intent);
     }
+
+    public void goToPart4(View view) {
+        Intent intent = new Intent(this, ExamineFearActivity.class);
+        startActivity(intent);
+    }
+
+
+
+    public static void addPoints(int extra) {
+        points += extra;
+    }
+
+
 }
