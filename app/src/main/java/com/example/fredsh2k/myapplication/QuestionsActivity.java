c2be24e257a4b842837734673e7b07a8ecbda911
Natali Levin <natalilevin72@gmail.com>
2018-05-27 15:10:42 +0300

    Added questions activity

--- app/src/main/java/com/example/fredsh2k/myapplication/QuestionsActivity.java
+++ app/src/main/java/com/example/fredsh2k/myapplication/QuestionsActivity.java
@@ -1,7 +1,9 @@
 package com.example.fredsh2k.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioGroup;
 
 public class QuestionsActivity extends AppCompatActivity {
@@ -13,5 +15,10 @@ public class QuestionsActivity extends AppCompatActivity {
 
         RadioGroup checkbox_age = findViewById(R.id.checkbox_age);
 //        checkbox_age.
+    }
+
+    public void goToMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
